b3cc6166a8d6626f1fd20dbe6e60052fed8b3e79
Ilya Bushmakin <im.bushmakin@gmail.com>
2023-04-12 17:22:00 +0000

    test(database): :test_tube: add fixtures for db tests
    added initialization code for db tests

---
+++ tests/conftest.py
@@ -0,0 +1,37 @@
+import asyncio
+import json
+import pytest
+from sqlalchemy import insert
+from src.database.db import Base, engine, async_session_maker
+from src.database.models import Account, Instrument, Operation, Transaction
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def prepare_database():
+    """Prepare database by importing all the test data."""
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    def open_mock_json(model: str):
+        with open(f"tests/mock_{model}.json") as file:
+            return json.load(file)
+
+    async with async_session_maker() as session:
+        insert_accounts = insert(Account).values(open_mock_json("accounts"))
+        insert_instruments = insert(Instrument).values(open_mock_json("instruments"))
+        insert_operations = insert(Operation).values(open_mock_json("operations"))
+        insert_transactions = insert(Transaction).values(open_mock_json("transactions"))
+
+        await session.execute(insert_accounts)
+        await session.execute(insert_instruments)
+        await session.execute(insert_operations)
+        await session.execute(insert_transactions)
+
+
+@pytest.fixture(scope="session")
+def event_loop(request):
+    """Create an instance of the default event loop for each test case."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
